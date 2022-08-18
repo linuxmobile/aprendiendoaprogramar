44ba51ae8290f0ea19ef02c5a13faf32af0c9c67
Braian A. Diez <bdiez19@gmail.com>
2022-08-18 18:41:40 -0300

    Feat: Improved some tips

--- .eleventy.js
+++ .eleventy.js
@@ -371,6 +371,19 @@ module.exports = function (eleventyConfig) {
         }
       },
     })
+    .use(mdItContainer, "idea", {
+      render: function (tokens, idx) {
+        var m = tokens[idx].info.trim().match(/^col-2-equal\s+(.*)$/);
+        if (tokens[idx].nesting === 1) {
+          return `<div class='idea alert'>
+                    <div class='_icon'>
+                      <img width='22' height='22' class='keep-original' src='/img_src/icons/idea.svg' alt='Idea icon'>
+                    </div>`;
+        } else {
+          return "</div>";
+        }
+      },
+    })
     .use(mdItContainer, "info", {
       render: function (tokens, idx) {
         var m = tokens[idx].info.trim().match(/^col-2-equal\s+(.*)$/);

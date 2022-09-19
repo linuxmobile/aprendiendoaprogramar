8c5cb2e7cb6e636f4fc64cd9bfbe19a61736d42e
linuxmobile <bdiez19@gmail.com>
2022-09-19 19:46:03 -0300

    Feat: Improve ud_site script

--- scripts/ud_site.sh
+++ scripts/ud_site.sh
@@ -1,15 +1,20 @@
-# echo "#### 1. Start building site"
-# npm run build
-echo "#### 2. Start copying aprendiendoaprogramar/_site to _site/_site/"
-rm -rf ../_site/_site
-mkdir ../_site/_site
-cp -Rf _site/*  ../_site/_site
-echo "End copying"
-echo "#### 3. Go to _site folder"
-cd ../_site/
-echo "#### 4. Start updating _site to Github"
+echo "1️⃣ 1. Updating notes..."
+cd ../notes
 git add .
 git commit -m "Updated: `date +'%Y-%m-%d %H:%M:%S'`"
 git push
-echo "End updating & come back to aprendiendoaprogramar"
-cd ../aprendiendoaprogramar/
+echo "⏰ 2. Wait 3 seconds..."
+sleep 3
+echo "2️⃣ 3. Updating submodules & current changes..."
+cd ../aprendiendoaprogramar
+git submodule update --remote
+git add .
+git commit -m "Updated: `date +'%Y-%m-%d %H:%M:%S'`"
+git push
+echo "⏰ 4. Wait 3 seconds..."
+sleep 3
+echo "3️⃣ 5. Merging dev to prod..."
+git push . head:prod
+echo "🚀 6. Updating prod & trigger build on netlify..."
+git push origin prod
+echo "✅ Done!"
